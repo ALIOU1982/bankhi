6a535bb847d74be3309ef4fcf4fa0533ffed1728
BARRY <aliou.barry@novasdux-solutions.com>
2020-10-29 15:02:24 +0100

    DAO Spring Boot

--- src/main/java/org/immo/bankhi/DAO/TypeImmoRepository.java
+++ src/main/java/org/immo/bankhi/DAO/TypeImmoRepository.java
@@ -2,7 +2,9 @@ package org.immo.bankhi.DAO;
 
 import org.immo.bankhi.entities.TypeImmo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TypeImmoRepository extends JpaRepository<TypeImmo, Long>{
 
 }
